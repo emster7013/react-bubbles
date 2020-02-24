37cf4a39805428f12d25827fee9a0730c02a0435
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-02-23 21:45:10 -0500

    added my login form and update my username and password in servers.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route } from 
+"react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
+import { BubblesContext } from "./context/BubblesContext";
+import Bubbles from "./components/Bubbles";
 
 function App() {
+  const [bubbles, setBubbles]=useState([]);
   return (
     <Router>
+      <BubblesContext.Provider value={Bubbles, setBubbles}>
       <div className="App">
         <Route exact path="/" component={Login} />
         {/* 
@@ -14,6 +18,7 @@ function App() {
           display BubblePage when you're authenticated 
         */}
       </div>
+      </BubblesContext.Provider>
     </Router>
   );
 }
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,62 @@
-import React from "react";
+import React, {useState} from "react";
+import axiosWithAuth from '../util/axiosWithAuth';
+import styled from 'styled-components';
 
-const Login = () => {
+const LoginDiv = styled.div `
+background: red;
+
+`
+
+const Login = props => {
+  const [loginz, setLoginz] = useState({
+    username:'',
+    password:''
+  })
+
+  const handleChange = e =>{
+    setLoginz({...loginz, [e.target.name]:
+       e.target.value})
+  }
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    axiosWithAuth() 
+    .put('/login', loginz)
+    .then(res =>{localStorage.setItem('token', res.data.payload);
+  setLoginz(loginz)
+  props.history.push('/colors')
+  })
+  .catch(err=>{console.log('inavlid login', err);
+  });
+  };
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    
+    <form onSubmit={handleSubmit}>
+      <LoginDiv>
+        <div>
+          <h1>Welcome to the Login!</h1>
+    <input
+      type="text"
+      name="username"
+      placeholder="username"
+      value={loginz.username}
+      onChange={handleChange}
+    />
+    <br/>
+    <input
+      type="password"
+      name="password"
+      placeholder="password"
+      value={loginz.password}
+      onChange={handleChange}
+    />
+    </div>
+    </LoginDiv>
+    <button>Log in</button>
+  </form>
+  
   );
 };
 
